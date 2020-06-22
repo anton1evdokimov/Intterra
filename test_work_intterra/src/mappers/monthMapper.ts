61146f674f14ceae3fd1a31f78a714d2e37ba8c1
anton1evdokimov <anton1evdokimov@yandex.ru>
2020-06-22 13:41:25 +0300

    mapper

---
+++ test_work_intterra/src/mappers/monthMapper.ts
@@ -0,0 +1,13 @@
+export const MonthMapper = new Map<number, string>();
+MonthMapper.set(0, "ЯНВ");
+MonthMapper.set(1, "ФЕВ");
+MonthMapper.set(2, "МАР");
+MonthMapper.set(3, "АПР");
+MonthMapper.set(4, "МАЯ");
+MonthMapper.set(5, "ИЮН");
+MonthMapper.set(6, "ИЮЛ");
+MonthMapper.set(7, "АВГ");
+MonthMapper.set(8, "СЕН");
+MonthMapper.set(9, "ОКТ");
+MonthMapper.set(10, "НОЯБ");
+MonthMapper.set(11, "ДЕК");
